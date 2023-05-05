619d038676df674f9ee1e75b622354040cab2a33
qrach <125902082+qrach@users.noreply.github.com>
2023-05-05 07:35:52 -0400

    Create edgebreakr.user.js

---
+++ edgebreakr.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @author       qrach
+// @name         EdgebreakR
+// @version      0.1.0
+// @description  Aims to automate most edgenuity tasks
+// @namespace    https://github.com/qrach/edgebreakr/
+// @downloadURL  https://raw.githubusercontent.com/qrach/edgebreakr/main/edgebreakr.user.js
+// @updateURL   https://raw.githubusercontent.com/qrach/edgebreakr/main/edgebreakr.user.js
+// @match        *://*.core.learn.edgenuity.com/*
+// @match        https://student.edgenuity.com/*
+// @grant        none
+// ==/UserScript==
